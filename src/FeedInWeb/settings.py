656668b792c94c281fef55ac67cf79115209269f
Keven Li <pbleester@gmail.com>
2015-05-07 17:38:22 +0800

    remove database dependent

--- src/FeedInWeb/settings.py
+++ src/FeedInWeb/settings.py
@@ -30,10 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
@@ -42,12 +39,8 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
@@ -60,14 +53,7 @@ WSGI_APPLICATION = 'FeedInWeb.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'feedinweb',
-        'USER': 'feedinweb',
-        'PASSWORD': 'feedinweb',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+
 }
 
 # Internationalization
--- src/FeedInWeb/urls.py
+++ src/FeedInWeb/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'FeedInWeb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('users.urls')),
     url(r'^feeds/', include('feeds.urls')),
     url(r'^$', 'pages.views.index', name='index'),
